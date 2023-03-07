e23a23f2ead359e105e037b167f95eb6039e8145
huylequang210 <huylequanq210@gmail.com>
2023-03-07 22:47:03 +0700

    add code

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
-from FlaskWebProject import app, db
+from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 import string, random
+from azure.storage.blob import BlockBlobService
 from flask_login import UserMixin
+from werkzeug.utils import secure_filename
+from flask import flash
+
+blob_container = app.config['BLOB_CONTAINER']
+blob_service = BlockBlobService(account_name=app.config['BLOB_ACCOUNT'], account_key=app.config['BLOB_STORAGE_KEY'])
 
 def id_generator(size=32, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -21,7 +27,11 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-    
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
@@ -31,3 +41,28 @@ class Post(db.Model):
     image_path = db.Column(db.String(100))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
+    def save_changes(self, form, file, userId, new=False):
+        self.title = form.title.data
+        self.author = form.author.data
+        self.body = form.body.data
+        self.user_id = userId
+
+        if file:
+            filename = secure_filename(file.filename);
+            fileextension = filename.rsplit('.',1)[1];
+            Randomfilename = id_generator();
+            filename = Randomfilename + '.' + fileextension;
+            try:
+                blob_service.create_blob_from_stream(blob_container, filename, file)
+                if(self.image_path):
+                    blob_service.delete_blob(blob_container, self.image_path)
+            except Exception:
+                flash(Exception)
+            self.image_path =  filename
+        if new:
+            db.session.add(self)
+        db.session.commit()
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
+
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'huylqstore'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'hiqm8G5eUnNgr2bQcq62+93gncvfiJzpMgwGfm4nCrncxehOnZ6I6f4DUZKZZ/hl+Qo78a0B94eD+AStfokmbQ=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
+
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'free-db-huylq25.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'free-db-huylq25'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'huylequang210'
@@ -8,3 +14,20 @@ class Config(object):
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + ':' + SQL_PASSWORD + '@' + SQL_SERVER + '/' + SQL_DATABASE + '?driver=ODBC+Driver+18+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    ### Info for MS Authentication ###
+    ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
+    CLIENT_SECRET = "AQr8Q~2dODSFuxrMmaGi8~DtNvRZkdfWCsFsRc87"
+
+    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
+
+    CLIENT_ID = "d621d96d-0688-4073-8f3d-745397b4df4f"
+
+    REDIRECT_PATH = "/getAToken"  # Used to form an absolute URL; must match to app's redirect_uri set in AAD
+
+    # You can find the proper permission names from this document
+    # https://docs.microsoft.com/en-us/graph/permissions-reference
+    SCOPE = ["User.Read"] # Only need to read user profile for this app
+
+    SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
